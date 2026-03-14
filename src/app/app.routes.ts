d8de1a505785896da2212511f0608984616970ac
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-03-14 23:53:08 +0100

    landing page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './shared/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+       path: '',
+       component: LandingPage   
+    }];
