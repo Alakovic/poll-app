ef11fefb9331e859d6fdb2e3c82de185008d47b1
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-03-19 17:20:06 +0100

    question-form stardet

--- src/app/shared/components/survey-form/survey-form.ts
+++ src/app/shared/components/survey-form/survey-form.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { QuestionForm } from '../question-form/question-form';
 
 @Component({
   selector: 'app-survey-form',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, QuestionForm],
   templateUrl: './survey-form.html',
   styleUrls: ['./survey-form.scss'],
 })
